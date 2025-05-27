64969f71b9541ac86560cfa2e6702daf5efed3ee
hansrajathole <hansrajathole1@gmail.com>
2025-05-27 23:35:01 +0530

    feat: implement data fetching and update UI in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [data, setData] = useState(null)
   const fetchData = async () => {
     try {
       const response = await fetch(`https://dummyjson.com/products?limit=50`)
@@ -8,7 +9,10 @@ const App = () => {
         throw new Error('Network response was not ok')
       }
       const data = await response.json()
-      console.log(data)
+      // console.log(data)
+      setData(data)
+      console.log('Data fetched successfully:', data);
+      
     }
     catch (error) {
       console.error('There has been a problem with your fetch operation:', error)
@@ -21,7 +25,11 @@ const App = () => {
   
   return (
     <div>
-      hello world
+      <h1>Fetch Data Example</h1>
+      <p>Check the console for fetched data.</p>
+      {
+
+      }
     </div>
   )
 }
