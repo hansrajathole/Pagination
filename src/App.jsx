90b38ec87fb2bce0a31ee7992d7dcc49fb0eac2b
hansrajathole <hansrajathole1@gmail.com>
2025-05-27 23:23:21 +0530

    feat: implement data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      console.log(data)
+    }
+    catch (error) {
+      console.error('There has been a problem with your fetch operation:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
   return (
     <div>
       hello world
