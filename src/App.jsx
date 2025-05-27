5ca5d3527017290597f13dee9ce943b19b0db55f
hansrajathole <hansrajathole1@gmail.com>
2025-05-27 23:26:38 +0530

    feat: update data fetching URL in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 const App = () => {
   const fetchData = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const response = await fetch(`https://dummyjson.com/products?limit=50`)
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
