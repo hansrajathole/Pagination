58bda631010b745079a5a90f1ad346d761fee5da
hansrajathole <hansrajathole1@gmail.com>
2025-05-29 03:00:23 +0530

    feat: implement Card component and update data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 const App = () => {
-  const [dataa, setData] = useState([])
+  const [data, setData] = useState([])
+  const [currentPage, setcurrentPage] = useState(0)
+  const pageSize = 12
   const fetchData = async () => {
-    try {
-      const response = await fetch(`https://dummyjson.com/products?limit=50`)
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-      const data = await response.json()
-      // console.log(data)
-      setData(data.products)
-      console.log('Data fetched successfully:', data);
-      
-    }
-    catch (error) {
-      console.error('There has been a problem with your fetch operation:', error)
-    }
-  }
+    const response = await fetch(`https://dummyjson.com/products?limit=500`);
+    const jsonData = await response.json();
+    setData(jsonData.products);
+    console.log("Data fetched successfully:", data);
+  };
+  let length = data.length
+  let noOfPages = Math.ceil(length/pageSize)
+  let start = currentPage * pageSize
+  let end = start + pageSize
 
   useEffect(() => {
-    fetchData()
-  }, [])
-  
-  return (
-    <div>
-      <h1>Fetch Data Example</h1>
-      <p>Check the console for fetched data.</p>
-      {
-        dataa?.map((item , idx) => 
-          <div key={idx}>
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-            <img src={item.images} alt={item.title} style={{ width: '100px', height: '100px' }} />
-          </div>
-        )
-      }
-    </div>
-  )
-}
+    fetchData();
+  }, []);
 
-export default App
+  const handlePage = (p)=>{
+    setcurrentPage(p)
+  }
+
+  return data.length < 0 ? (
+    <div>No data Found</div>
+  ) : (
+    <div className="App">
+      <div className="pagination-text">Pagination.....</div>
+      <div className="card-container">
+        {data.slice(start, end).map((prod) => (
+          <Card key={prod.id} image={prod.thumbnail} title={prod.title} />
+        ))}
+      </div>
+      <div className="page-container">
+        {
+          [...Array(noOfPages).keys()].map((p)=> <span key={p} onClick={()=>handlePage(p)} className="page">{p}</span> )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default App;
